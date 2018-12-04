d00a3f742fa23f1277d0bfe3ae9daf25e9724571
float <zexiplus@outlook.com>
2018-12-04 13:48:39 +0800

    update offcial website add dynamic script to import tvvm.js

---
+++ website/scripts/doc.js
@@ -0,0 +1,16 @@
+script.onload = function () {
+  new TVVM({
+    el: '#doc',
+    data: function () {
+      return {
+        navList: [{
+          title: '666',
+          url: '123'
+        }]
+      }
+    },
+    methods: {
+
+    }
+  })
+}
